a98aaa4ac8a01fcd8a10138e97360cb24e468d3e
Cleone Jacob <therealcleonejacob@gmail.com>
2017-01-27 04:09:36 -0500

    make it better

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '04aa7b78726647e37e62ba16f84fee3d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
